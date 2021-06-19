81e9f2d6fea96e6b8c272625c079709dc3bf098c
shivam585 <85876404+shivam585@users.noreply.github.com>
2021-06-19 07:56:48 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -45,7 +45,7 @@ function setup() {
   
  back=createSprite(width/2,height/2,1000000,100000);
 back.addImage(backimage)
-back.scale = 50;
+back.scale = 20;
   
   trex = createSprite(50,180,20,50);
   trex.addAnimation("running", trex_running);
